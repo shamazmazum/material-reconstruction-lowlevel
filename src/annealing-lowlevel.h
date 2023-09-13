2eccb85183537de488f0a787c6460f271be4383c
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-09-13 10:23:54 +0300

    Export only external symbols

--- src/annealing-lowlevel.h
+++ src/annealing-lowlevel.h
@@ -1,17 +1,21 @@
 #pragma once
 
-/* Fourier transform */
-int an_rfft (const float         *array,
-             float               *real,
-             float               *imag,
-             const unsigned int  *dimensions,
-             unsigned int         ndims);
+#define AN_EXPORT __attribute__((visibility ("default")))
 
-int an_irfft (float              *array,
-              const float        *real,
-              const float        *imag,
-              const unsigned int *dimensions,
-              unsigned int        ndims);
+/* Fourier transform */
+AN_EXPORT int
+an_rfft (const float         *array,
+         float               *real,
+         float               *imag,
+         const unsigned int  *dimensions,
+         unsigned int         ndims);
+
+AN_EXPORT int
+an_irfft (float              *array,
+          const float        *real,
+          const float        *imag,
+          const unsigned int *dimensions,
+          unsigned int        ndims);
 
 #define MAX_DIMENSIONS 3
 
@@ -20,52 +24,52 @@ struct an_image;
 struct an_corrfn;
 struct an_metric;
 
-struct an_gpu_context*
+AN_EXPORT struct an_gpu_context*
 an_create_context(unsigned int ndim, int validation);
 
-void
+AN_EXPORT void
 an_destroy_context (struct an_gpu_context *ctx);
 
-struct an_image*
+AN_EXPORT struct an_image*
 an_create_image (struct an_gpu_context *ctx,
                  const float           *real,
                  const float           *imag,
                  const unsigned int    *dimensions,
                  unsigned int           ndim);
 
-void
+AN_EXPORT void
 an_destroy_image (struct an_image *image);
 
-struct an_corrfn*
+AN_EXPORT struct an_corrfn*
 an_create_corrfn (struct an_gpu_context *ctx,
                   const float           *corrfn,
                   const unsigned int    *dimensions,
                   unsigned int           ndim);
 
-void
+AN_EXPORT void
 an_destroy_corrfn (struct an_corrfn *corrfn);
 
-int
+AN_EXPORT int
 an_image_update_fft (struct an_image    *image,
                      const unsigned int *coord,
                      unsigned int        ndim,
                      float               delta);
 
 /* Testing */
-int
+AN_EXPORT int
 an_image_get (struct an_image *image,
               float           *real,
               float           *imag);
 
 /* Distance measurement */
-struct an_metric*
+AN_EXPORT struct an_metric*
 an_create_metric (struct an_gpu_context *ctx,
                   struct an_corrfn      *target,
                   struct an_image       *recon);
 
-void
+AN_EXPORT void
 an_destroy_metric (struct an_metric *metric);
 
-int
+AN_EXPORT int
 an_distance (struct an_metric *metric,
              float            *distance);
